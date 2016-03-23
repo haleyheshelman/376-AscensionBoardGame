0de1124a12d8ff990462089852241d723a2a2cbd
fenogljc <fenogljc@rose-hulman.edu>
2016-03-22 21:05:33 -0400

    testGetCardCost works

--- src/main/java/devops/hw1/core/Card.java
+++ src/main/java/devops/hw1/core/Card.java
@@ -9,6 +9,8 @@ package devops.hw1.core;
  */
 public class Card {
 
+	private int cost;
+
 	/**
 	 * constructor, makes a Card.
 	 */
@@ -17,11 +19,35 @@ public class Card {
 	}
 	
 	/**
+	 * @param i
+	 */
+	private Card(int i) {
+		this.cost = i;
+	}
+
+	/**
 	 * calls the constructor of Card and returns the card
 	 * @return the Card
 	 */
 	public static Card makeCard() {
 		Card newCard = new Card();
+		return newCard;
+	}
+
+	/**
+	 * @return
+	 */
+	public int getCost() {
+		// TODO Auto-generated method stub
+		return this.cost;
+	}
+
+	/**
+	 * @param i
+	 * @return
+	 */
+	public static Card makeCard(int i) {
+		Card newCard = new Card(i);
 		return newCard;
 	}
 	
