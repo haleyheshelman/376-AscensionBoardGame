5213755a3e3fd882d837bd92a6f224b65340c6bc
fenogljc <fenogljc@rose-hulman.edu>
2016-03-31 21:14:41 -0400

    added initialization for the effects hashmap

--- src/main/java/devops/hw1/core/ACard.java
+++ src/main/java/devops/hw1/core/ACard.java
@@ -7,41 +7,42 @@ import java.util.HashMap;
 
 /**
  * @author fenogljc
- *
+ * 
  */
 public abstract class ACard {
 
 	protected int cost;
-	protected HashMap<String,Integer> effects;
-	
+	protected HashMap<String, Integer> effects = new HashMap<String, Integer>();
+
 	public static ACard makeCard() {
 		return new DrawCard();
 	}
-	
+
 	public static ACard makeCard(int i) {
 		return new DrawCard(i);
 	}
 
 	/**
 	 * Returns that cost of the card.
+	 * 
 	 * @return
 	 */
 	public int getCost() {
 		return this.cost;
 	}
-	
+
 	/**
 	 * Sets the cost of the card.
+	 * 
 	 * @return
 	 */
 	public boolean setCost(int i) {
 		this.cost = i;
 		return true;
 	}
-	
-	public HashMap<String,Integer> getEffects(){
+
+	public HashMap<String, Integer> getEffects() {
 		return this.effects;
 	}
-	
-}
 
+}
