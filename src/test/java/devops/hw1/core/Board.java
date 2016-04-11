4827815df64df4bd6db420601d00af061fe1a89b
fenogljc <fenogljc@rose-hulman.edu>
2016-04-11 16:53:09 -0400

    makeBoard works (woohoo) test passed

---
+++ src/test/java/devops/hw1/core/Board.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package devops.hw1.core;
+
+/**
+ * @author fenogljc
+ * @author gneezyn
+ * 
+ */
+public class Board {
+
+	private Board() {
+
+	}
+	
+	public static Board makeBoard(){
+		return new Board();
+	}
+}
