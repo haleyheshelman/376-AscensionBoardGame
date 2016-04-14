5e9b11ca7c03a2c81988a05633fd585bf75b671f
fenogljc <fenogljc@rose-hulman.edu>
2016-04-14 03:47:23 -0400

    testCompleteCardMaker passes

--- src/main/java/devops/hw1/core/HeroCard.java
+++ src/main/java/devops/hw1/core/HeroCard.java
@@ -1,5 +1,7 @@
 package devops.hw1.core;
 
+import java.util.HashMap;
+
 public class HeroCard extends ACard {
 	
 	public HeroCard(){
@@ -8,6 +10,26 @@ public class HeroCard extends ACard {
 	
 	public HeroCard(int i){
 		this.cost = i;
+	}
+
+	/**
+	 * @param image
+	 * @param name
+	 * @param faction
+	 * @param cost
+	 * @param honor
+	 * @param rarity
+	 * @param effects
+	 */
+	public HeroCard(String image, String name, String faction, int cost,
+			int honor, int rarity, HashMap<String, Integer> effects) {
+		this.image = image;
+		this.name = name;
+		this.faction = faction;
+		this.cost = cost;
+		this.honor = honor;
+		this.rarity = rarity;
+		this.effects = effects;
 	}
 
 }
