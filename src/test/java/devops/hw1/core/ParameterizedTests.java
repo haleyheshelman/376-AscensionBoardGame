83887c021c2e22e2276164895af85d9aab144a58
gneezyn <gneezyn@rose-hulman.edu>
2016-04-21 12:50:44 -0400

    Parameterized tests now pass (testing buyCard). Might add more later

--- src/test/java/devops/hw1/core/ParameterizedTests.java
+++ src/test/java/devops/hw1/core/ParameterizedTests.java
@@ -7,48 +7,70 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.runners.Parameterized;
 import org.junit.runner.RunWith;
-import org.easymock.EasyMock;
 
 import static org.junit.Assert.*;
 
+/**
+ * Parameterized Tests of some of the Unit Tests (found in Tests.java).
+ * 
+ * @author gneezyn
+ *
+ */
 @RunWith(Parameterized.class)
 public class ParameterizedTests {
 	
-	private ACard fakeCard = EasyMock.niceMock(ACard.class);
-	private Integer inputNumber;
-	private Boolean expectedResult;
+//	private ACard fakeCard = EasyMock.niceMock(ACard.class);
+	private Integer cardCost;
+	private Integer expectedRunes;
+	private Integer expectedDiscSize;
+	private Boolean bool;
 	private Player player1;
 
 	@Before
     public void initialize() {
       player1 = Player.makePlayer();
-      player1.addRunes(3); // start the player off with some runes to use to buy a card
+      player1.addRunes(10); // start the player off with some runes to use to buy a card (
    }
-
+	
    // Each parameter should be placed as an argument here
    // Every time runner triggers, it will pass the arguments
    // from parameters we defined in paramNumbers() method (below)
-   public ParameterizedTests(Integer cardCost, 
-      Boolean expectedResult) {
-      this.inputNumber = inputNumber;
-      this.expectedResult = expectedResult;
+	 /**
+	 * @param cardCost the cost of the Card (in Runes)
+	 * @param expectedRunes the number of runes expected after calling buyCard
+	 * @param expectedDiscardSize the expected size of the player's discard pile after calling buyCard
+	 * @param bool indicates whether testing for true or false (as result of calling buyCard)
+	 */
+	public ParameterizedTests(Integer cardCost, 
+      Integer expectedRunes, Integer expectedDiscardSize, Boolean bool) {
+      this.cardCost = cardCost;
+      this.expectedRunes = expectedRunes;
+      this.expectedDiscSize = expectedDiscardSize;
+      this.bool = bool;
    }
 
    @Parameterized.Parameters
    public static Collection paramNumbers() {
       return Arrays.asList(new Object[][] {
-         { 2, true },
-         { 6, false },
-         { 3, true },
-         { 4, false }
+         { 6, 4, 1, true },
+         { 3, 7, 1, true },
+         { 11, 10, 0, false }
       });
    }
    
    @Test
    public void testBuyCardParameterized() {
-	   System.out.println("Parameterized Number is: " + inputNumber);
-	   assertEquals(expectedResult, player1.buyCard(fakeCard));
-//	   assertTrue(expectedResult == player1.getPower());
+	   ACard card = ACard.makeCard(cardCost);
+	   
+	   if (bool) {
+		   assertTrue(player1.buyCard(card));		   
+		   assertTrue(player1.getDiscardPile().contains(card));
+	   } else {
+		   assertFalse(player1.buyCard(card));
+		   assertFalse(player1.getDiscardPile().contains(card));
+	   }
+	   assertTrue(player1.getRunes() == expectedRunes);
+	   assertTrue(player1.getDiscardSize() == expectedDiscSize);
    }
 
 }
