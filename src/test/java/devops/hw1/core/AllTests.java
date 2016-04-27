dd5f9744f7442bd21f3d47d941f6269228347c46
gneezyn <gneezyn@rose-hulman.edu>
2016-04-27 18:35:43 -0400

    Added a class to run all tests
    Should help with using CodeCover

---
+++ src/test/java/devops/hw1/core/AllTests.java
@@ -0,0 +1,13 @@
+package devops.hw1.core;
+
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ParameterizedTestBuyCard.class, ParamTestMonsterError.class, Tests.class})
+public class AllTests {
+	public static void main (String args[]) {
+        org.junit.runner.JUnitCore.main("devops.hw1.core.AllTests");
+    }
+}
