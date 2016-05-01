f73b76b3b29ed41d1e3ef607e6c4ec7243723f30
gneezyn <gneezyn@rose-hulman.edu>
2016-04-30 23:20:58 -0400

    added the images for the cpu's hand (doesn't actually do anything yet)

--- src/main/java/devops/hw1/core/GameBoard.java
+++ src/main/java/devops/hw1/core/GameBoard.java
@@ -7,8 +7,11 @@ import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Shape;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -37,25 +40,22 @@ public class GameBoard extends JPanel {
 	public JLabel discard_pileLabel = new JLabel("Discard: 0");
 	public JButton endTurnButton = new JButton("End Turn");
 
-	// counters for keeping track of the various information, will be replaced
-	// as project progresses
-	public int runeCount;
-	public int powerCount;
-	public int deckCount;
-	public int discardCount;
-	public int honorCount;
-
 	// tracking positions of cards based on rectangle objects drawn beneath them
 	public ArrayList<Shape> centerList;
 	public ArrayList<Shape> playerList;
+	public ArrayList<Shape> cpuList;
 
 	public Player player;
 	public Board board;
+	public Player cpu;
 
-	// sets bounds for player hand
+	// sets bounds for the player's hand
 	public int playerHandLeft = 300;
 	public int playerHandRight = WIDTH - 300;
 	public int playerHandY = HEIGHT - 200;
+	
+	// sets bounds for the cpu's hand (same as player's hand, except for the height)
+	public int cpuHandY = 50;
 
 	// sets positions for the center cards
 	public int cardY = HEIGHT - 500;
@@ -68,14 +68,9 @@ public class GameBoard extends JPanel {
 		// first call constructor of superclass
 		super();
 
-		// then initialize value
-		this.runeCount = 0;
-		this.powerCount = 0;
-		this.deckCount = 5;
-		this.discardCount = 0;
-
 		this.centerList = new ArrayList<Shape>();
 		this.playerList = new ArrayList<Shape>();
+		this.cpuList = new ArrayList<Shape>();
 
 		// Setting the fonts of the labels
 		this.runesLabel.setFont(LABEL_FONT);
@@ -106,9 +101,8 @@ public class GameBoard extends JPanel {
 
 		// then do additional stuff
 		this.player = player;
-		this.runeCount = this.player.getRunes();
-		this.discardCount = this.player.getDiscardSize();
 		this.board = board;
+		this.cpu = Player.makePlayer();
 		this.endTurnButton.addActionListener(new EndTurnListener(this.player,
 				this));
 	}
@@ -121,6 +115,7 @@ public class GameBoard extends JPanel {
 		super.paint(g);
 		Graphics2D g2 = (Graphics2D) g;
 
+		// setting up the player's hand
 		int numCards = player.getHandSize();
 		if (numCards != 0) {
 			int factor = (playerHandRight - playerHandLeft) / numCards;
@@ -130,16 +125,32 @@ public class GameBoard extends JPanel {
 
 			}
 		}
+		
+		// setting up the center field (cards)
 		for (int j = 0; j < board.getCenField().length; j++) {
 			BufferedImage card = board.getCenField()[j].getImage();
 			g2.drawImage(card, card1X + (100 * j), cardY, null);
 
 		}
 
+		// painting the player's discard pile
 		if (player.getDiscardSize() != 0) {
 			g2.drawImage(
 					player.getDiscardPile().get(player.getDiscardSize() - 1)
 							.getImage(), 20, 600, null);
+		}
+		
+		int cpuCards = cpu.getHandSize();
+		if (cpuCards != 0) {
+			int factor = (playerHandRight - playerHandLeft) / cpuCards;
+			for (int i = 0; i < cpuCards; i++) {
+				try {
+					BufferedImage card = ImageIO.read(new File("cardImages/back_of_card.png"));
+					g2.drawImage(card, 300 + (factor * i), cpuHandY, null);
+				} catch (IOException e) {
+					System.out.println("The image was not found at: " + "cardImages/back_of_card.png");
+				}
+			}
 		}
 
 	}
